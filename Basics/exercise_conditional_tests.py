93caf506ba610ab6c0111d88bc6ec308ef760b75
Razor87 <sergrazor87@gmail.com>
2017-07-16 20:02:49 +0300

    Updated

--- Basics/exercise_conditional_tests.py
+++ Basics/exercise_conditional_tests.py
@@ -1,4 +1,5 @@
 car = 'Toyota'
+print("car = 'Toyota'")
 print("Is car == 'subaru'? I predict False.")
 print(car == 'subaru')
 print("Is car == 'audi'? I predict False.")
@@ -13,61 +14,67 @@ print("Is car != 'toyota'? I predict False.")
 print(car.lower() != 'toyota')
 
 year = 2017
-print("\nIs year == '2000'? I predict False.")
+print('\nyear = 2017')
+print("Is year == 2000? I predict False.")
 print(year == 2000)
-print("Is year == '2017'? I predict True.")
+print("Is year == 2017? I predict True.")
 print(year == 2017)
-print("Is year != '2017'? I predict True.")
+print("Is year != 2017? I predict True.")
 print(year != 2000)
-print("Is year != '2000'? I predict False.")
+print("Is year != 2000? I predict False.")
 print(year != 2017)
-print("Is year > '2017'? I predict True.")
+print("Is year > 2017? I predict True.")
 print(year > 2000)
-print("Is year > '2000'? I predict False.")
+print("Is year > 2000? I predict False.")
 print(year > 2018)
-print("Is year >= '2017'? I predict True.")
+print("Is year >= 2017? I predict True.")
 print(year >= 2000)
-print("Is year >= '2017'? I predict True.")
+print("Is year >= 2017? I predict True.")
 print(year >= 2017)
-print("Is year < '2000'? I predict False.")
+print("Is year < 2000? I predict False.")
 print(year < 2000)
-print("Is year < '2017'? I predict True.")
+print("Is year < 2017? I predict True.")
 print(year < 2018)
-print("Is year <= '2000'? I predict False.")
+print("Is year <= 2000? I predict False.")
 print(year <= 2000)
-print("Is year <= '2017'? I predict True.")
+print("Is year <= 2017? I predict True.")
 print(year <= 2017)
 
-print("\nIs year == '2017' and != '2000'? I predict True.")
+print("\nIs year == 2017 and != 2000? I predict True.")
 print(year == 2017 and year != 2000)
-print("Is year > '2010' and != '2000'? I predict True.")
+print("Is year > 2010 and != 2000? I predict True.")
 print(year > 2010 and year != 2000)
-print("Is year < '2020' and != '2017'? I predict False.")
+print("Is year < 2020 and != 2017? I predict False.")
 print(year < 2020 and year != 2017)
-print("Is year < '2020' and >= '2017'? I predict True.")
+print("Is year < 2020 and >= 2017? I predict True.")
 print(year < 2020 and year >= 2017)
 
-print("\nIs year == '2017' or != '2017'? I predict True.")
+print("\nIs year == 2017 or != 2017? I predict True.")
 print(year == 2017 or year != 2017)
-print("Is year != '2016' or < '2016'? I predict True.")
+print("Is year != 2016 or < 2016? I predict True.")
 print(year != 2016 or year < 2016)
 
-leap_years = ['2000', '2004', '2008', '2012', '2016', '2020', '2024', '2028']
+leap_years = [2000, 2004, 2008, 2012, 2016, 2020, 2024, 2028]
+print('\nLeap years:')
+print(leap_years)
 year = 2001
-print("\nIs year in leap_years? I predict False.")
+print('\nyear = 2001')
+print("Is this year a leap year? I predict False.")
 print(year in leap_years)
 year = 2004
-print("Is year in leap_years? I predict True.")
+print('year = 2004')
+print("Is this year a leap year? I predict True.")
 print(year in leap_years)
-
-year = '2005'
-print("\nIs year not in leap_years? I predict True.")
+year = 2005
+print('year = 2005')
+print("This year is not a leap year? I predict True.")
 if year not in leap_years:
     print('True')
 else:
     print('False')
-year = '2012'
-print("Is year not in leap_years? I predict False.")
+year = 2012
+print('year = 2012')
+print("This year is not a leap year? I predict False.")
 if year not in leap_years:
     print('True')
 else:
---
+++ Basics/exercise_if_lists.py
@@ -0,0 +1,9 @@
+usernames = ['john11', 'user99', 'admin', 'tester55', 'guest22']
+if usernames:
+    for username in usernames:
+        if username == 'admin':
+            print("\nHello Admin, would you like to see a status report?")
+        else:
+            print("\nHello " + username.title() + ", thank you for logging in again.")
+else:
+    print("We need to find some users!")
