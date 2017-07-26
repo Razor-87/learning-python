271cfb764a8b98e74574a37047169ad549cca26e
Razor87 <sergrazor87@gmail.com>
2017-07-26 13:38:26 +0300

    Transfer of files with exercises to the folder Exercises.

---
+++ Basics/Exercises/exercise_def_function_6.py
@@ -0,0 +1,20 @@
+def show_magicians(names):
+    """Displays a name from the list."""
+    print("\nMagicians: ")
+    for name in names:
+        print(name.title())
+
+
+def make_great(names):
+    """Added 'Great'"""
+    for name in names:
+        great = 'Great '
+        name2 = names.pop()
+        names.insert(0, great + name2)
+    return names
+
+
+magicians = ['phil', 'robert', 'ted']
+magicians2 = make_great(magicians[:])
+show_magicians(magicians2)
+show_magicians(magicians)
