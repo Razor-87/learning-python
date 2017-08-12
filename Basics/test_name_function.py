29f1af13703e9d0c1edaf55131a5e6c9c32b8dce
Razor87 <sergrazor87@gmail.com>
2017-08-12 19:46:19 +0300

    Started "Chapter 11. Testing your code"

---
+++ Basics/test_name_function.py
@@ -0,0 +1,8 @@
+# 12.07.2017
+def get_formatted_name(first, last, middle=''):
+    """Return a full name, neatly formatted."""
+    if middle:
+        full_name = first + ' ' + middle + ' ' + last
+    else:
+        full_name = first + ' ' + last
+    return full_name.title()
