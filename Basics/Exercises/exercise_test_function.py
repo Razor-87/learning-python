b227da0ec4328fe2bd3ebd3dab187932578ef82c
Razor87 <sergrazor87@gmail.com>
2017-08-13 17:51:55 +0300

    Updated "Chapter 11. Testing your code."

---
+++ Basics/Exercises/exercise_test_function.py
@@ -0,0 +1,9 @@
+# 13.07.2017
+def city_country(city, country, population=''):
+    """Displays the city and country."""
+    if population:
+        population = " - population " + str(population) + "."
+        city_country = city.title() + ", " + country.title() + population
+    else:
+        city_country = city.title() + ", " + country.title()
+    return city_country
---
+++ Basics/Exercises/exercise_testing.py
@@ -0,0 +1,20 @@
+# 13.07.2017
+import unittest
+from exercise_test_function import city_country
+
+
+class CityCountryTest(unittest.TestCase):
+    """Tests for 'exercise_test_function.py'."""
+
+    def test_city_country(self):
+        """Verify that the function call is correct."""
+        test_cc = city_country('santiago', 'chile')
+        self.assertEqual(test_cc, 'Santiago, Chile')
+
+    def test_city_country_population(self):
+        """Verify that the function call is correct."""
+        test_ccp = city_country('santiago', 'chile', 5000000)
+        self.assertEqual(test_ccp, 'Santiago, Chile - population 5000000.')
+
+
+unittest.main()
