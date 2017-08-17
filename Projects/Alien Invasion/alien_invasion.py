7d0153bccc7089e5315d14ec5817d459edef53ee
Razor87 <sergrazor87@gmail.com>
2017-08-17 19:12:14 +0300

    Updated "Projects: Alien Invasion."

--- Projects/Alien Invasion/alien_invasion.py
+++ Projects/Alien Invasion/alien_invasion.py
@@ -1,26 +1,30 @@
-# 15.08.2017
-# Starting the Game Project
-import sys
-
+# 17.08.2017
 import pygame
+
+from settings import Settings
+from ship import Ship
+from galaxy import Galaxy
+import game_functions as gf
 
 
 def run_game():
-    """Initialize game and create a screen object."""
+    # Initialize game, settings, and screen object.
     pygame.init()
-    screen = pygame.display.set_mode((1200, 800))
+    ai_settings = Settings()
+    screen = pygame.display.set_mode(
+        (ai_settings.screen_width, ai_settings.screen_height))
     pygame.display.set_caption("Alien Invasion")
+
+    # Make a ship
+    ship = Ship(screen)
+
+    # Make a galaxy
+    galaxy = Galaxy(screen)
 
     # Start the main loop for the game.
     while True:
-
-        # Watch for keyboard and mouse events.
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                sys.exit()
-
-        # Make the most recently drawn screen visible.
-        pygame.display.flip()
+        gf.check_events()
+        gf.update_screen(ai_settings, screen, ship, galaxy)
 
 
 run_game()
