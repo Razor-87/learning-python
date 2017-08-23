919700894d368ac53e9a5ebe84d90099a0d4ef1a
Razor87 <sergrazor87@gmail.com>
2017-08-23 20:32:02 +0300

    Updated "Projects: Alien Invasion."

--- Projects/Pygame/Exercises/exercise_12_5/game_functions.py
+++ Projects/Pygame/Exercises/exercise_12_5/game_functions.py
@@ -5,21 +5,21 @@ import pygame
 from bullet import Bullet
 
 
-def check_keydown_events(event, ai_settings, screen, ship, bullets):
+def check_keydown_events(event, ss_settings, screen, ship, bullets):
     """Respond to keypresses."""
     if event.key == pygame.K_RIGHT:
         ship.moving_right = True
     elif event.key == pygame.K_LEFT:
         ship.moving_left = True
     elif event.key == pygame.K_SPACE:
-        fire_bullet(ai_settings, screen, ship, bullets)
+        fire_bullet(ss_settings, screen, ship, bullets)
 
 
-def fire_bullet(ai_settings, screen, ship, bullets):
+def fire_bullet(ss_settings, screen, ship, bullets):
     """Fire a bullet if limit not reached yet."""
     # Create a new bullet and add it to the bullets group.
-    if len(bullets) < ai_settings.bullets_allowed:
-        new_bullet = Bullet(ai_settings, screen, ship)
+    if len(bullets) < ss_settings.bullets_allowed:
+        new_bullet = Bullet(ss_settings, screen, ship)
         bullets.add(new_bullet)
 
 
@@ -31,22 +31,22 @@ def check_keyup_events(event, ship):
         ship.moving_left = False
 
 
-def check_events(ai_settings, screen, ship, bullets):
+def check_events(ss_settings, screen, ship, bullets):
     """Respond to keypresses and mouse events."""
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
 
         elif event.type == pygame.KEYDOWN:
-            check_keydown_events(event, ai_settings, screen, ship, bullets)
+            check_keydown_events(event, ss_settings, screen, ship, bullets)
         elif event.type == pygame.KEYUP:
             check_keyup_events(event, ship)
 
 
-def update_screen(ai_settings, screen, ship, bullets):
+def update_screen(ss_settings, screen, ship, bullets):
     """Update images on the screen and flip to the new screen."""
     # Redraw the screen during each pass through the loop.
-    screen.fill(ai_settings.bg_color)
+    screen.fill(ss_settings.bg_color)
     # Redraw all bullets behind ship and aliens.
     for bullet in bullets.sprites():
         bullet.draw_bullet()
