0038a0dc017d6188838b2dec603deaf9a082bc62
Razor87 <sergrazor87@gmail.com>
2017-08-24 20:47:51 +0300

    Updated "Projects: Data Visualization."

--- Projects/Data Visualization/Matplotlib/rw_visual.py
+++ Projects/Data Visualization/Matplotlib/rw_visual.py
@@ -1,10 +1,32 @@
-# 21.08.2017
+# 24.08.2017
 import matplotlib.pyplot as plt
 
 from random_walk import RandomWalk
 
-# Make a random walk, and plot the points.
-rw = RandomWalk()
-rw.fill_walk()
-plt.scatter(rw.x_values, rw.y_values, s=10)
-plt.show()
+# Keeping making new walks, as long as the program active.
+while True:
+    # Make a random walk, and plot the points.
+    rw = RandomWalk(50000)
+    rw.fill_walk()
+
+    # Set the size of the plotting window.
+    plt.figure(dpi=128, figsize=(10, 6))
+
+    point_numbers = list(range(rw.num_points))
+    plt.scatter(rw.x_values, rw.y_values, c=point_numbers, cmap=plt.cm.Blues,
+                edgecolor='none', s=1)
+
+    # Emphasize the first and last points.
+    plt.scatter(0, 0, c='green', edgecolor='none', s=40)
+    plt.scatter(rw.x_values[-1], rw.y_values[-1], c='red', edgecolor='none',
+                s=40)
+
+    # Remove the axes.
+    plt.axes().get_xaxis().set_visible(False)
+    plt.axes().get_yaxis().set_visible(False)
+
+    plt.show()
+
+    keep_running = input("Make another walk? (y/n): ")
+    if keep_running == 'n':
+        break
