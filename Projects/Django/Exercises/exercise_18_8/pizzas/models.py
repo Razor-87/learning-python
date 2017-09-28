42cf7df86929d8c787c41fbf66420186b43e4fa1
Razor87 <sergrazor87@gmail.com>
2017-09-28 20:41:24 +0300

    Updated "Projects: Learning Log."

---
+++ Projects/Django/Exercises/exercise_18_8/pizzas/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+
+
+class Pizza(models.Model):
+    """Defines a model pizza."""
+    name = models.CharField(max_length=200)
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        """Return a string representation of the model."""
+        return self.name
+
+
+class Topping(models.Model):
+    """Defines a model called Topping."""
+    pizza = models.ForeignKey(Pizza)
+    name = models.CharField(max_length=200)
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        """Return a string representation of the model."""
+        return self.name
