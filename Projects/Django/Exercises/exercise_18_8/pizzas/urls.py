baeee37329ae017bc38110adbcfd0167039d6df3
Razor87 <sergrazor87@gmail.com>
2017-09-29 15:56:46 +0300

    Updated "Projects: Learning Log."

--- Projects/Django/Exercises/exercise_18_8/pizzas/urls.py
+++ Projects/Django/Exercises/exercise_18_8/pizzas/urls.py
@@ -7,4 +7,10 @@ from . import views
 urlpatterns = [
     # Home page
     url(r'^$', views.index, name='index'),
+
+    # Show all pizzas.
+    url(r'^pizzas/$', views.pizzas, name='pizzas'),
+
+    # Detail page for a single pizza.
+    url(r'^pizzas/(?P<pizza_id>\d+)/$', views.pizza, name='pizza'),
 ]
